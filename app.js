e55ac96582828d1ff11786281946cc8ba9d2548e
adarsh-av13 <dra4474@gmail.com>
2020-07-12 12:58:45 +0530

    basic notification set up

---
+++ app.js
@@ -0,0 +1,40 @@
+const config = require('./config');
+const express = require('express');
+const notifier = require('node-notifier');
+const path = require('path');
+const twit = require('twit');
+
+const app = express();
+const twitter = twit(config);
+
+const official_fpl = 761568335138058240;
+const params = { follow: official_fpl, language: 'en' };
+
+const stream = twitter.stream('statuses/filter', params);
+
+stream.on('connect', (request) => {
+    console.log("Connecting....");
+});
+
+stream.on('connected', (response) => {
+    console.log("Connected");
+});
+
+stream.on('error', (error) => {
+    console.log(error);
+});
+
+stream.on('tweet', (tweet) => {
+    let isFromRightUser = tweet.user.id == official_fpl;
+    let isRelevantTweet = isFromRightUser && (tweet.text.search("BONUS POINTS") != -1 || tweet.text.search("Goal - ") != -1);
+    if (isRelevantTweet) {
+        notifier.notify({
+            title: 'FPL Update',
+            message: tweet.text,
+            icon: path.join(__dirname, 'notification_icon.png'),
+            sound: true,
+        });
+    }
+});
+
+app.listen(3000);
